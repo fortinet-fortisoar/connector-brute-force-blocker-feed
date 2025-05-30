20fa53491ace7ee0baff8b5adde8ff3cfdb379fc
Smorankar <smorankar@shuklaamacOS 15.0.1 Shukla's Fortinet MacBook ProAbhishek Shukla>
2025-05-30 09:56:46 +0530

    First Commit

--- brute-force-blocker-feed/operations.py
+++ brute-force-blocker-feed/operations.py
@@ -1,11 +1,18 @@
-""" Copyright start
-  Copyright (C) 2008 - 2022 Fortinet Inc.
-  All rights reserved.
-  FORTINET CONFIDENTIAL & FORTINET PROPRIETARY SOURCE CODE
-  Copyright end """
+"""
+Copyright start
+MIT License
+Copyright (c) 2025 Fortinet Inc
+Copyright end
+"""
 
-import requests, datetime, time
+import requests, time, json
+import os.path
+import uuid
+
+from datetime import datetime
+
 from connectors.core.connector import get_logger, ConnectorError
+from django.conf import settings
 
 logger = get_logger('brute-force-blocker-feed')
 
@@ -51,9 +58,39 @@ class BruteForceBlockerFeed(object):
             raise ConnectorError(str(err))
 
 
+def get_ingestion_base_dir(**kwargs):
+    tenant_id = ''
+    request = kwargs.get("request")
+
+    if request and hasattr(request, 'tenantid'):
+        tenant_id = request.tenantid
+
+    base_indicator_dir = '/tmp/'  # default
+
+    try:
+        if hasattr(settings, 'ALL_CONFIG') and settings.ALL_CONFIG:
+            app_config = settings.ALL_CONFIG.get('application', {})
+            if isinstance(app_config, dict):
+                configured_dir = app_config.get('tenant_pv_base_dir', '/tmp/')
+                if '{tenant_id}' in configured_dir and tenant_id:
+                    base_indicator_dir = configured_dir.format(tenant_id=tenant_id)
+                else:
+                    base_indicator_dir = configured_dir
+    except Exception as e:
+        logger.warning(f"Error accessing settings.ALL_CONFIG: {e}")
+        base_indicator_dir = '/tmp/'
+
+    try:
+        os.makedirs(base_indicator_dir, exist_ok=True)
+    except Exception as e:
+        logger.warning(f"Could not create directory {base_indicator_dir}: {e}")
+        base_indicator_dir = '/tmp/'
+    return base_indicator_dir
+
+
 def convert_datetime_to_epoch(date_time):
     d1 = time.strptime(date_time, "%Y-%m-%dT%H:%M:%S.%fZ")
-    epoch = datetime.datetime.fromtimestamp(time.mktime(d1)).strftime('%s')
+    epoch = datetime.fromtimestamp(time.mktime(d1)).strftime('%s')
     return epoch
 
 
@@ -91,11 +128,49 @@ def fetch_indicators(config, params, **kwargs):
             return ips_list
 
 
+def download_indicators(config, params, **kwargs):
+    sf = BruteForceBlockerFeed(config)
+    config_id = config.get('config_id')
+    endpoint = ""
+    last_pull_time = params.get('last_pull_time')
+    response = sf.make_rest_call(endpoint, 'GET')
+    if response:
+        ip_blocklist = str(response).split("\\n")
+        last_modified_datetime = ip_blocklist[0].replace("\\t", " ").split(" ")[5].split(":")[1]
+        if last_pull_time:
+            last_pull_time = int(convert_datetime_to_epoch(last_pull_time))
+            if int(last_modified_datetime) > last_pull_time:
+                ips_list = find_indicators(ip_blocklist, last_modified_datetime)
+            else:
+                ips_list = []
+        else:
+            ips_list = find_indicators(ip_blocklist, last_modified_datetime)
+        base_indicator_dir = get_ingestion_base_dir(**kwargs)
+        try:
+            os.makedirs(base_indicator_dir, exist_ok=True)
+        except Exception as e:
+            base_indicator_dir = '/tmp/'
+            logger.warn("Not able to create dir for downloading indicators")
+
+        config_dir = base_indicator_dir + config_id + '/'
+        try:
+            os.makedirs(config_dir, exist_ok=True)
+        except Exception as e:
+            pass
+        file_name = str(uuid.uuid4()) + '.json'
+        file_path = os.path.join(config_dir, file_name)
+        with open(file_path, "w") as json_file:
+            json.dump(ips_list, json_file, indent=2)
+
+        return {"files": [file_path.replace(base_indicator_dir, '')], "last_pull_datetime": datetime.now()}
+
+
 def _check_health(config):
     sf = BruteForceBlockerFeed(config)
     return True
 
 
 operations = {
-    'fetch_indicators': fetch_indicators
+    'fetch_indicators': fetch_indicators,
+    'download_indicators': download_indicators
 }
